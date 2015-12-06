58cdc38f37094d3085598b7046f0defc6517c21b
Mateusz Jaje <mateuszjaje@gmail.com>
2015-12-06 15:54:16 +0100

    typo

--- czysc.sh
+++ czysc.sh
@@ -4,8 +4,8 @@
 IDS=`qstat | grep $USER | awk '{print $1}'`
 CNT=`qstat | grep $USER | wc -l`
 
-for ID in $IDS; do
-    qdel ID
+for ID in ${IDS}; do
+    qdel ${sID}
 done
 
 echo "Removed $CNT tasks. Yay!"
