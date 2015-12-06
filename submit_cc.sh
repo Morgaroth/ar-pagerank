bcc892b780af0c8bfaaf6fcd199a85c3dfab3e56
Mateusz Jaje <mateuszjaje@gmail.com>
2015-12-07 00:13:03 +0100

    updated submit script

--- submit_cc.sh
+++ submit_cc.sh
@@ -1,14 +1,6 @@
 #!/bin/env bash
 #PBS -A plgmorgaroth2015b
-
 module load plgrid/apps/spark
-
 start-multinode-spark-cluster.sh
-
-#export SPARK_CONF_DIR="$PROJ_HOME/src/main/resources/log4j.properties"
-
-netstat -at | grep 7077
-
-$SPARK_HOME/bin/spark-submit --master spark://`hostname`:7077 --class ConnectedComponents $PROJ_HOME/target/scala-2.10/sparkpagerank_2.10-1.0.jar $GRAPH_URI 1000
-
+SPARK_CONF_DIR="$PROJ_HOME/src/main/resources/" $SPARK_HOME/bin/spark-submit --master spark://`hostname`:7077 --class ConnectedComponents $PROJ_HOME/target/scala-2.10/sparkpagerank_2.10-1.0.jar $GRAPH_URI 1000
 stop-multinode-spark-cluster.sh
