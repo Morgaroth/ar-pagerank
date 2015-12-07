8b7d4fda493aac20727da164d0a10266ab35688c
Mateusz Jaje <mateuszjaje@gmail.com>
2015-12-07 19:53:25 +0100

    typo

--- submit_cc.sh
+++ submit_cc.sh
@@ -3,7 +3,7 @@
 module load plgrid/apps/spark
 start-multinode-spark-cluster.sh
 cd $PROJ_HOME
-$SPARK_HOME/bin/spark-submit --master spark://`hostname`:7077 --class ConnectedComponents $PROJ_HOME/target/scala-2.10/sparkpagerank_2.10-1.0.jar $GRAPH_URI 1000 > 1_${NODES}.csv
-$SPARK_HOME/bin/spark-submit --master spark://`hostname`:7077 --class ConnectedComponentsV2 $PROJ_HOME/target/scala-2.10/sparkpagerank_2.10-1.0.jar $GRAPH_URI 1000 > 2_${NODES}.csv
-$SPARK_HOME/bin/spark-submit --master spark://`hostname`:7077 --class ConnectedComponentsV3 $PROJ_HOME/target/scala-2.10/sparkpagerank_2.10-1.0.jar $GRAPH_URI 1000 > 3_${NODES}.csv
+$SPARK_HOME/bin/spark-submit --master spark://`hostname`:7077 --class ConnectedComponents $PROJ_HOME/target/scala-2.10/sparkpagerank_2.10-1.0.jar $GRAPH_URI 1000 >> 1_${NODES}.csv
+$SPARK_HOME/bin/spark-submit --master spark://`hostname`:7077 --class ConnectedComponentsV2 $PROJ_HOME/target/scala-2.10/sparkpagerank_2.10-1.0.jar $GRAPH_URI 1000 >> 2_${NODES}.csv
+$SPARK_HOME/bin/spark-submit --master spark://`hostname`:7077 --class ConnectedComponentsV3 $PROJ_HOME/target/scala-2.10/sparkpagerank_2.10-1.0.jar $GRAPH_URI 1000 >> 3_${NODES}.csv
 stop-multinode-spark-cluster.sh
